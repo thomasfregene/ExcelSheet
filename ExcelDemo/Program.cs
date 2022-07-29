a7168b1cceee95d09a581d7fccdd76881d027017
ThomasFregene <fregzythomas@gmail.com>
2022-07-29 12:23:05 +0100

    initial commit

---
+++ ExcelDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using OfficeOpenXml;
+using System;
+using System.IO;
+
+namespace ExcelDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+            var file = new FileInfo(@"C:\Demo\ExcelYoutubeDemo.xlsx");
+        }
+    }
+}
