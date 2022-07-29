9133648681524c568e8871f45442885640e2af73
ThomasFregene <fregzythomas@gmail.com>
2022-07-29 16:05:34 +0100

    read from excel

--- ExcelDemo/Program.cs
+++ ExcelDemo/Program.cs
@@ -1,15 +1,102 @@
 ﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ExcelDemo
 {
     partial class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             ExcelPackage.LicenseContext = LicenseContext.Commercial;
-            var file = new FileInfo(@"C:\Demo\ExcelYoutubeDemo.xlsx");
+            var file = new FileInfo(@"C:\Repo\Demo\ExcelYoutubeDemo.xlsx");
+
+            var people = GetSetupData();
+
+            //await SaveExcelFile(people, file);
+
+            List<PersonModel> peopleFromExcel = await LoadExcelFile(file);
+
+            foreach (var p in peopleFromExcel)
+            {
+                Console.WriteLine($"{p.Id} {p.FirstName} {p.LastName}");
+            }
+        }
+
+        private static async Task<List<PersonModel>> LoadExcelFile(FileInfo file)
+        {
+            List<PersonModel> output = new();
+            using var package = new ExcelPackage();
+            await package.LoadAsync(file);
+            var ws = package.Workbook.Worksheets[0];
+
+            int row = 3;
+            int col = 1;
+
+            while (string.IsNullOrWhiteSpace(ws.Cells[row,col].Value?.ToString()) == false)
+            {
+                PersonModel p = new();
+                p.Id = int.Parse(ws.Cells[row, col].Value?.ToString());
+                p.FirstName = ws.Cells[row, col + 1].Value.ToString();
+                p.LastName = ws.Cells[row, col + 2].Value.ToString();
+
+                output.Add(p);
+                row += 1;
+            }
+
+            return output;
+        }
+
+        private static async Task SaveExcelFile(List<PersonModel> people, FileInfo file)
+        {
+            DeleteIfExist(file);
+
+            using var package = new ExcelPackage(file);
+
+            var ws = package.Workbook.Worksheets.Add("MainReport");
+            var range = ws.Cells["A2"].LoadFromCollection(people,true);
+            range.AutoFitColumns();
+
+            //Formats the Header
+            ws.Cells["A1"].Value = "Our Cool Report";
+            ws.Cells["A1:C1"].Merge = true;
+            ws.Column(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            //A1 row
+            ws.Row(1).Style.Font.Size = 24;
+            ws.Row(1).Style.Font.Color.SetColor(Color.Blue);
+
+            ws.Row(2).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            ws.Row(2).Style.Font.Bold = true;
+            ws.Column(3).Width = 20;
+
+            await package.SaveAsync();
+
+
+           
+        }
+
+        private static void DeleteIfExist(FileInfo file)
+        {
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+        }
+
+        static List<PersonModel> GetSetupData()
+        {
+            List<PersonModel> output = new()
+            {
+                new() { Id = 1, FirstName = "John", LastName = "Doe" },
+                new() { Id = 2, FirstName = "Jane", LastName = "Doe" },
+                new() { Id = 3, FirstName = "Sue", LastName = "Smith" }
+            };
+
+            return output;
         }
     }
 }
